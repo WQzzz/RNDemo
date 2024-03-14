9ea544514ada30584d0b826719d8f47c31b91fcf
WQZzz <weiqi.zheng@thoughtworks.com>
2024-03-14 16:18:04 +0800

    try ScrollView

--- App.tsx
+++ App.tsx
@@ -18,15 +18,18 @@ import {
   SafeAreaView,
   View,
   Modal,
+  ScrollView,
+  Switch,
   TouchableWithoutFeedback
 } from 'react-native';
+
 
 
 function InputPage(){
   const [ModalVisible,setModalVisible]=useState(false)
   return(
   <SafeAreaView style={styles.container} >
-      <Modal visible={ModalVisible} transparent={false} animationType="slide">
+      <Modal visible={ModalVisible} transparent={true} animationType="slide">
         <View style={{justifyContent:"center",flex:1,alignItems:"center"}}>
         <View style={styles.modal}>
           <Text>This is a Modal</Text>
@@ -36,6 +39,20 @@ function InputPage(){
         </View>
       </Modal>
       <Button title="show Modal" onPress={()=>{setModalVisible(true)}}></Button>
+      <ScrollView style={{flex:1}}>
+      <Text style={{fontSize:42,margin:20,backgroundColor:"yellow"}}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+      </Text>
+    </ScrollView>
+    <Switch value={true}>
+
+    </Switch>
     <KeyboardAvoidingView style={{flex:1,justifyContent:"center"}} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
       <Text>Please enter somethings</Text>
       <TextInput keyboradType="default" style={{borderWidth:1}}></TextInput>
@@ -44,6 +61,7 @@ function InputPage(){
   </SafeAreaView>
   )
 }
+
 
 function App(): React.JSX.Element {
   return <InputPage/>;
@@ -56,6 +74,7 @@ const styles = StyleSheet.create({
     height:180,
     width:180,
     borderWidth:1,
+    backgroundColor:"white"
   },
   container: {
     flex: 1,
