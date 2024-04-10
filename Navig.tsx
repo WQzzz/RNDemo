195da4e0220503520e18ceff511e5f0406a3cd40
WQZzz <weiqi.zheng@thoughtworks.com>
2024-04-10 11:47:03 +0800

    Feat: modify ndk verision and modify font

--- Navig.tsx
+++ Navig.tsx
@@ -5,14 +5,26 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 const Tab = createBottomTabNavigator()
 
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+// (...)
 import {FlatListScreen} from './MessageFlatList'
 import {SearchTagScreen,SearchTagSectionList} from './SearchTagSectionList';
 import {PimsScreen} from './PimsSectionList'
 
 function Navig() {
     return (
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName;
+            iconName = focused ? 'list' : 'list-outline';
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={30} color="pink" />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}>
         <Tab.Screen name="FlatList" component={FlatListScreen} />
         {/* <Tab.Screen name="SectionList" component={SearchTagScreen} /> */}
         <Tab.Screen name="PimsSection" component={PimsScreen}/>
