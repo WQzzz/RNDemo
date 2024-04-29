54dc3f616b6504248c42dcfb9646644aa5c31404
WQZzz <weiqi.zheng@thoughtworks.com>
2024-04-29 16:01:21 +0800

    feat： configure jest test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  preset: 'react-native',
-};
+    preset: 'react-native',
+    transformIgnorePatterns: [
+      'node_modules/(?!react-native-payfort-sdk|react-native)/',
+    ],
+    globals: {
+      __DEV__: true,
+    },
+    testEnvironment: 'node',
+    setupFiles: ["./jestSetupFile.js",],
+  };
